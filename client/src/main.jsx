f3c65b327a81f4621e9d6eddcc8a080df707fa93
Mohammad Rameez <mdrameez7245@gmail.com>
2026-06-30 00:07:46 +0530

    Fix GitHub Pages routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,8 +13,8 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <BrowserRouter basename="/LMS_Website">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/LMS_Website/">
       <AppContextProvider>
         <App />
       </AppContextProvider>
